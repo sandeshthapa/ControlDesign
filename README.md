466cd1a57afab00937a2c4e78a3470e7f195b4b4
Sandesh Thapa <thapasandesh1@gmail.com>
2022-01-24 19:55:26 -0500

    Added a CMakeList

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # simplePID
 
 A simple controller to calcuate forces. 
+
+#### Install 
+Clone the repository 
+
+
+```
+git clone https://github.com/sandeshthapa/simplePID
+cd simplePID 
+cd build 
+cmake ..
+make 
+./simple_pid
+```
---
+++ build/CMakeFiles/simple_pid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_pid.dir/PID.cpp.o"
+  "simple_pid.pdb"
+  "simple_pid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_pid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
